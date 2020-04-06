662c08c6c03df11bdeae609c437b18ce2eaeff6a
suhaib-bizongo <suhaib.ahmed@bizongo.com>
2020-04-06 08:55:22 +0530

    ui

--- html/jsFilesForRestCall/getLogin.js
+++ html/jsFilesForRestCall/getLogin.js
@@ -1,8 +1,121 @@
-$(document).ready(function() {
+function login(username, password){
+
+
+
     $.ajax({
-        url: "https://artworkapi.bizongo.com/login"
-    }).then(function(data) {
-       $('.id').append(data.id);
-       $('.password').append(data.password);
-    });
+    type: "POST",
+    url: "https://secret-crag-28491.herokuapp.com/https://artworkapi.bizongo.com/login",
+    beforeSend: function (xhr) {
+    xhr.setRequestHeader ("Authorization", "Basic " + btoa($('#login_form_username').val() + ":" + $('#login_form_password').val()));
+  },
+    success: function (data){
+
+            console.log("hello");
+            console.log(data);
+            setCookie("jwt", data.jwt_token, 1)
+
+            alert(getCookie("jwt"))
+
+            setdetails();
+
+            
+
+
+          },
+          failure : function (data){
+            console.log("helloo");
+            console.log(data);
+        
+          }
 });
+
+
+}
+
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+
+
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
+    }
+}
+
+
+function openmytasks(){
+              window.location.href = "mytasks.html";
+}
+
+
+function setmytasks(){
+
+    $.ajax({
+    type: "GET",
+    url: "https://secret-crag-28491.herokuapp.com/https://artworkapi.bizongo.com/companies/"+getCookie("company_id")+"/task_dashboard",
+    beforeSend: function (xhr) {
+    xhr.setRequestHeader ("Authorization", "Basic " + btoa($('#login_form_username').val() + ":" + $('#login_form_password').val()));
+  },
+    success: function (data){
+            console.log("hello");
+            console.log(data);
+            console.log(data)
+
+          },
+          failure : function (data){
+            console.log("helloo");
+            console.log(data);
+        
+          }
+
+})
+
+}
+
+function setdetails(){
+
+
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    
+    if (this.readyState == 4 && this.status == 200) {
+
+      data = JSON.parse(this.responseText);
+      setCookie("name",data.name,1)
+      setCookie("company_id", data.company_id,1)
+      window.location.href = "chooseoption.html";
+    }
+    else{
+
+    }
+  };
+  
+  xhttp.open("GET", "https://secret-crag-28491.herokuapp.com/https://artworkapi.bizongo.com/users/me", true);
+  xhttp.setRequestHeader("Authorization", "Bearer " + getCookie("jwt"));
+  
+  xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+  xhttp.send()
+
+
+}
+
+
+
+function logout(){
+
+  setCookie("jwt", "",1);
+  window.location.href = "index.html";
+
+}
--- html/jsFilesForRestCall/listArtWork.js
+++ html/jsFilesForRestCall/listArtWork.js
@@ -1,7 +1,36 @@
-$.ajax({
-    url: "https://artworkapi.bizongo.com/library/artworks/library/artworks",
-    dataType: 'json',
-		headers: {
-	    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJBcnR3b3JrIiwidXNlcl9pZCI6MzY0NDYsInJvbGVzIjpbImRlZmF1bHQiXSwiaXNzIjoiVXNlciBNYW5hZ2VtZW50IFN5c3RlbSIsImxhc3RfbmFtZSI6IkFkaXR5YSIsImV4cCI6MTU4ODY3NjM2NCwiZmlyc3RfbmFtZSI6IkF0dWwiLCJlbWFpbCI6ImF0dWwuYWRpdHlhQGJpem9uZ28uY29tIiwidGltZXN0YW1wIjoxNTg2MDE0NzY3fQ.UJ45jx9M5WBZ8FWgCT-HsNHTQvR5DM99rG5QZBVFj4Y"
-	  }
-});
+function setmytasks(){
+
+    $.ajax({
+    type: "GET",
+    url: "https://secret-crag-28491.herokuapp.com/https://artworkapi.bizongo.com/companies/"+getCookie("company_id")+"/task_dashboard",
+    beforeSend: function (xhr) {
+    xhr.setRequestHeader ("Authorization", "Bearer " + getCookie("jwt"));
+  },
+    success: function (data){
+            console.log("hello");
+            console.log(data)
+            data.data.forEach(function(d){
+
+              var markup = "<tr>"
+                            +"<td style='text-align: center;' class='ant-col ant-col-6'  scope='col'>"+d.type+"</td>"
+                            +"<td style='text-align: center;' class='ant-col ant-col-6'  scope='col'>"+d.attributes.project.name+"</td>"
+                            +"<td style='text-align: center;' class='ant-col ant-col-6'  scope='col'>"+d.attributes.stage_due_date+"</td>"
+                            +"<td style='text-align: center;' class='ant-col ant-col-6'  scope='col'><a><button onclick='uploadartworktask()' type='button' class='ant-btn login-form-button ant-btn-primary ant-btn-block'><span>Upload</span></button></a></td>"
+                        +"</tr>";
+                      
+
+
+
+            $("table tbody").append(markup);            })
+          },
+          failure : function (data){
+            console.log("helloo");
+            console.log(data);
+             window.location.href = "index.html";
+        
+          }
+
+})
+}
+
+setmytasks();

e7af35d686de4e54a7574d1aa69c17d75719fb91
Atul Aditya <atul.aditya@bizongo.com>
2020-04-14 18:36:31 +0530

    Push the first uploadable api in working condition , without any encoding error

--- html/jsFilesForRestCall/index.js
+++ html/jsFilesForRestCall/index.js
@@ -27,69 +27,128 @@ var pdfButton = document.querySelector("#export-pdf");
 /*
   Event listeners
 */
-pdfButton.addEventListener('click', function(){exportWithType('pdf');}, false);
+pdfButton.addEventListener(
+  "click",
+  function () {
+    exportWithType("pdf");
+  },
+  false
+);
 // jpgButton.addEventListener('click', function(){exportWithType('jpg');}, false);
 // pngButton.addEventListener('click', function(){exportWithType('png');}, false);
 // originalButton.addEventListener('click', function(){exportWithType();}, false);
 
 function exportWithType(type) {
-		csInterface.evalScript(`exportFile("${type}")`, function(path) {
-			alert("file saved at " + path);
+	console.log(type);
+    csInterface.evalScript(`exportFile("${type}")`, function (path) {
+    console.log("file saved at " + path);
 
-			// var filePath = app.activeDocument.fullName;
-			// var splitPath = filePath.toString().split('.');
-			// var originalExtension = splitPath[splitPath.length-1];
-			// var folderPath = filePath.toString().split('/').slice(0,-1).join('/') + '/';
-			// var sanitizedFilePath = File(filePath).fsName;
-			var xhr = new XMLHttpRequest();
-			var url = "https://artwork-qa1-temp.s3.ap-south-1.amazonaws.com";
-			xhr.open("POST", url, true);
-			//xhr.setRequestHeader("Content-Type", "application/json");
-			xhr.onreadystatechange = function () {
-				    alert("success_action_status "+xhr.response);
-			    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-			        alert(xhr.response);
-			    }
-			};
-			var formData = new FormData();
-			//TODO:: craete a file object so that we can do a post call. on that.
-			//var file = window.cep.fs.readFile(path);
-			var file = window.cep.fs.readFile(path,cep.encoding.utf8).data;
-			//var file = File(path);
-			//alert(file); 
-			//alert(file.data);
-			// result = window.cep.fs.readFile(path, cep.encoding.utf8);
-			// var file;
-			// //alert(result.err);
-			// if (result.err === 0) {
-			// 	alert("No error");
-			// 	var base64Data = result.data;
-			// 	alert("base64Data" +base64Data);
-			// 	file = cep.encoding.convertion.b64_to_utf8(base64Data);
-			// 	alert(file);
-			// }
-			// else {
-			//  	alert("error");
-			// }
+    // var filePath = app.activeDocument.fullName;
+    // var splitPath = filePath.toString().split('.');
+    // var originalExtension = splitPath[splitPath.length-1];
+    // var folderPath = filePath.toString().split('/').slice(0,-1).join('/') + '/';
+    // var sanitizedFilePath = File(filePath).fsName;
+    // var xhr = new XMLHttpRequest();
+    // var url = "https://artwork-qa1-temp.s3.ap-south-1.amazonaws.com";
+    // xhr.open("POST", url, true);
+    // //xhr.setRequestHeader("Content-Type", "application/json");
+    // xhr.onreadystatechange = function () {
+    // 	    console.log("success_action_status "+xhr.response);
+    //     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //         console.log(xhr.response);
+    //     }
+    // };
+    // var formData = new FormData();
+    // //TODO:: craete a file object so that we can do a post call. on that.
+    // //var file = window.cep.fs.readFile(path);
+    // var fs = require('fs');
+    // var file = fs.createReadStream(path);
+    //var file = File(path);
+    //console.log(file);
+    //console.log(file.data);
+    // result = window.cep.fs.readFile(path, cep.encoding.utf8);
+    // var file;
+    // //console.log(result.err);
+    // if (result.err === 0) {
+    // 	console.log("No error");
+    // 	var base64Data = result.data;
+    // 	console.log("base64Data" +base64Data);
+    // 	file = cep.encoding.convertion.b64_to_utf8(base64Data);
+    // 	console.log(file);
+    // }
+    // else {
+    //  	console.log("error");
+    // }
 
-			
-			var data = {"key": "d00c354e-8908-4917-b7df-9f57dca96838",
+    //console.log(file);
+    //console.log(file.data);
+    /*
+			var data = {"key": "afb181c7-c8ff-4f4d-817e-7b52b076b1a3",
                 "success_action_status": "201",
                 "x-amz-meta-original-filename": "atul_a_1.pdf",
                 "Content-Type": "application/pdf",
-                "policy": "eyJleHBpcmF0aW9uIjoiMjAyMC0wNC0wOVQxMjoyNjoxNloiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJhcnR3b3JrLXFhMS10ZW1wIn0seyJrZXkiOiJkMDBjMzU0ZS04OTA4LTQ5MTctYjdkZi05ZjU3ZGNhOTY4MzgifSx7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6IjIwMSJ9LHsieC1hbXotbWV0YS1vcmlnaW5hbC1maWxlbmFtZSI6ImF0dWxfYV8xLnBkZiJ9LHsiQ29udGVudC1UeXBlIjoiYXBwbGljYXRpb24vcGRmIn0seyJ4LWFtei1jcmVkZW50aWFsIjoiQUtJQVRDNkpZWU1JNk5XRFpQNlMvMjAyMDA0MDkvYXAtc291dGgtMS9zMy9hd3M0X3JlcXVlc3QifSx7IngtYW16LWFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IngtYW16LWRhdGUiOiIyMDIwMDQwOVQxMTU2MTZaIn1dfQ==",
-                "x-amz-credential": "AKIATC6JYYMI6NWDZP6S/20200409/ap-south-1/s3/aws4_request",
+                "policy": "eyJleHBpcmF0aW9uIjoiMjAyMC0wNC0xM1QxNToxMDowMVoiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJhcnR3b3JrLXFhMS10ZW1wIn0seyJrZXkiOiJhZmIxODFjNy1jOGZmLTRmNGQtODE3ZS03YjUyYjA3NmIxYTMifSx7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6IjIwMSJ9LHsieC1hbXotbWV0YS1vcmlnaW5hbC1maWxlbmFtZSI6ImF0dWxfYV8xLnBkZiJ9LHsiQ29udGVudC1UeXBlIjoiYXBwbGljYXRpb24vcGRmIn0seyJ4LWFtei1jcmVkZW50aWFsIjoiQUtJQVRDNkpZWU1JNk5XRFpQNlMvMjAyMDA0MTMvYXAtc291dGgtMS9zMy9hd3M0X3JlcXVlc3QifSx7IngtYW16LWFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IngtYW16LWRhdGUiOiIyMDIwMDQxM1QxNDQwMDFaIn1dfQ==",
+                "x-amz-credential": "AKIATC6JYYMI6NWDZP6S/20200413/ap-south-1/s3/aws4_request",
                 "x-amz-algorithm": "AWS4-HMAC-SHA256",
-                "x-amz-date": "20200409T115616Z",
-                "x-amz-signature": "f329ae53bdfac0fc93a1df4210014fe90d63edc0e31ec5dcf0d0c92a80402315"
+                "x-amz-date": "20200413T144001Z",
+                "x-amz-signature": "5d349ea1c9c06df62ef82b724567b56345ea86c4de7d9543b31ff02dd227876d"
 	            	};
 	        Object.keys(data).forEach(key => formData.append(key, data[key]));
 	        formData.append('file', file);
-	        alert(formData);
+	        console.log(formData);
 			xhr.send(formData);
 			xhr.onerror = function(error) {
-	  			alert("Request failed "+error);
+	  			console.log("Request failed "+error);
 			};
 
-		});
+		}); 
+		
+           */
+
+
+	console.log("require fs request");
+    var fs = require('fs');
+	var request = require('request');
+	//require('form-data');
+	//Node is giving error while installing , looking into it. 
+	console.log("require");
+    var data = {
+		"key": "bfa3559a-5f28-43d7-87c9-011bf8e9f888",
+        "success_action_status": "201",
+		"x-amz-meta-original-filename": "atul_a_1.pdf",
+		"Content-Type": "application/pdf",
+		"policy": "eyJleHBpcmF0aW9uIjoiMjAyMC0wNC0xNFQxMTowODo0N1oiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJhcnR3b3JrLXFhMS10ZW1wIn0seyJrZXkiOiJiZmEzNTU5YS01ZjI4LTQzZDctODdjOS0wMTFiZjhlOWY4ODgifSx7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6IjIwMSJ9LHsieC1hbXotbWV0YS1vcmlnaW5hbC1maWxlbmFtZSI6ImF0dWxfYV8xLnBkZiJ9LHsiQ29udGVudC1UeXBlIjoiYXBwbGljYXRpb24vcGRmIn0seyJ4LWFtei1jcmVkZW50aWFsIjoiQUtJQVRDNkpZWU1JNk5XRFpQNlMvMjAyMDA0MTQvYXAtc291dGgtMS9zMy9hd3M0X3JlcXVlc3QifSx7IngtYW16LWFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IngtYW16LWRhdGUiOiIyMDIwMDQxNFQxMDM4NDdaIn1dfQ==",
+		"x-amz-credential": "AKIATC6JYYMI6NWDZP6S/20200414/ap-south-1/s3/aws4_request",
+		"x-amz-algorithm": "AWS4-HMAC-SHA256",
+		"x-amz-date": "20200414T103847Z",
+		"x-amz-signature": "ce607888e6569ef2b1c052399a8b3b5d9eefb1f2659dbf694006d88879b888cf",
+       file: fs.createReadStream(path)
+    };
+    // var formData;
+    // Object.keys(data).forEach(key => formData.append(key, data[key]));
+    // console.log(formData);
+    // // formData = {
+    // // file: {
+    // //     value:  fs.createReadStream(path),
+    // //     options: {
+    // //       filename: 'atul_a_1.pdf',
+    // //       contentType: 'application/pdf'
+    // //     }
+    // //   }
+    // // };
+    // formData.append(file , fs.createReadStream(path));
+    console.log(data);
+    request.post(
+      {
+        url: "https://artwork-qa1-temp.s3.ap-south-1.amazonaws.com",
+        formData: data
+      },
+      function optionalCallback(err, httpResponse, body) {
+        if (err) {
+          console.log("upload failed: " + err);
+        }
+        console.log("Upload successful!  Server responded with: " + body);
+      }
+    );
+  });
 }
